f842997d15a7c0441474ca247b7a86368b344b89
Erik Haliewicz <ehal256@gmail.com>
2022-01-14 16:58:43 -0800

    remove reference to logo

--- res/graphics_res.h
+++ res/graphics_res.h
@@ -5,7 +5,6 @@ extern const Palette pal;
 extern const SpriteDefinition fire_fixup;
 extern const SpriteDefinition bottom_line_cover;
 extern const SpriteDefinition shotgun;
-extern const Image doom_logo;
 extern const Image skybox_gradient;
 extern const Image hud;
 
