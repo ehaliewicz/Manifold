1fa84d2d886dc98a433d1685765ef75a06f9d715
Erik Haliewicz <ehal256@gmail.com>
2022-02-04 11:38:40 -0800

    remove unused graphics

--- res/graphics_res.h
+++ res/graphics_res.h
@@ -2,8 +2,6 @@
 #define _RES_GRAPHICS_RES_H_
 
 extern const Palette pal;
-extern const SpriteDefinition fire_fixup;
-extern const SpriteDefinition bottom_line_cover;
 extern const SpriteDefinition shotgun;
 extern const Image skybox_gradient;
 extern const Image hud;
--- res/graphics_res.res
+++ res/graphics_res.res
@@ -1,6 +1,4 @@
 
-SPRITE fire_fixup "fire_fixup.bmp" 4 1 0 0 NONE TILE
-SPRITE bottom_line_cover "bottom_line_cover.bmp" 12 1 0 0 NONE TILE
 PALETTE pal "gritty_palette.png" NONE
 IMAGE skybox_gradient "images/SKYBOX1.png" NONE ALL
 IMAGE hud "images/HUD.png" NONE ALL
