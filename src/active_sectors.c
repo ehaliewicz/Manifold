fa9850873d127378b63b6a53622c8a25853d832f
Erik Haliewicz <ehal256@gmail.com>
2022-02-04 11:40:00 -0800

    start working on more efficient active sector tracking

---
+++ src/active_sectors.c
@@ -0,0 +1,48 @@
+#include <genesis.h>
+#include "active_sectors.h"
+#include "portal_map.h"
+
+
+u32* active_sectors;
+u16 num_longwords;
+
+void init_active_sectors() {
+    active_sectors = MEM_alloc(MAX_SECTOR_GROUPS/8);
+    num_longwords = MAX_SECTOR_GROUPS/32;
+}
+
+void cleanup_active_sectors() {
+    MEM_free(active_sectors);
+}
+
+void register_sect_group_as_active(u16 sect_group) {
+    u16 lw_index = sect_group>>5;
+    u8 bit_index = sect_group & 0b111;
+    active_sectors[lw_index] |= (1 << bit_index);
+}
+
+void register_sect_group_as_inactive(u16 sect_group) {
+    u16 lw_index = sect_group>>5;
+    u8 bit_index = sect_group & 0b111;
+    active_sectors[lw_index] &= ~(1 << bit_index);
+}
+
+
+void iterate_active_sectors(u16 num_sector_groups, active_sector_callback cb) {
+    int b = 0;
+
+    int bit_index = 0;
+    for(u16 lw_index = 0; lw_index < num_longwords; lw_index++) {
+        u32 lw = active_sectors[lw_index];
+        if(lw == 0) { continue; }
+
+        u16 base_index = lw_index<<5;
+
+        for(u32 bit_index = 0; bit_index < 32; bit_index++) {
+            if(lw & (1<<bit_index)) {
+                u16 sect_group_index = base_index+bit_index;
+                cb(sect_group_index);
+            }
+        }
+    }
+}
---
+++ src/active_sectors.h
@@ -0,0 +1,19 @@
+#ifndef ACTIVE_SECTORS
+#define ACTIVE_SECTORS
+
+#include <genesis.h>
+
+typedef void(*active_sector_callback)(u16 sect_group);
+
+
+void init_active_sectors();
+
+void cleanup_active_sectors();
+
+void register_sect_group_as_active(u16 sect_group);
+
+void register_sect_group_as_inactive(u16 sect_group);
+
+void iterate_active_sectors(u16 num_sector_groups, active_sector_callback cb);
+
+#endif
