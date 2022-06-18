d647bb427c9e73b25b3408d16a1b53e597f85828
Erik Haliewicz <ehal256@gmail.com>
2022-06-17 17:40:01 -0700

    add cleanbuild

--- build.bat
+++ build.bat
@@ -10,6 +10,10 @@ IF "x%1" == "x" (
   %GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen debug
   %GENS% %CD%\out\rom.bin -D
   REM %GENS_KMOD% %CD%\out\rom.bin
+) ELSE IF "%1" == "cleanbuild" (
+  
+  %GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen clean
+  %GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen release
 ) ELSE (
   %GDK_WIN%\bin\make -f %GDK_WIN%\makefile.gen %1
 ) 
