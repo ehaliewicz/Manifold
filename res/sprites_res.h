6fc0fc8e6d86e8cf9e8cf663217b3cfb19a2be60
Erik Haliewicz <ehal256@gmail.com>
2022-07-31 22:30:48 -0700

    more resource stuff

---
+++ res/sprites_res.h
@@ -0,0 +1,7 @@
+#ifndef _RES_SPRITES_RES_H_
+#define _RES_SPRITES_RES_H_
+
+extern const Palette sprite_pal;
+extern const SpriteDefinition shotgun;
+
+#endif // _RES_SPRITES_RES_H_
---
+++ res/sprites_res.res
@@ -0,0 +1,2 @@
+SPRITE shotgun "images/SHOTGUN.png" 8 8 0 0 NONE TILE
+PALETTE sprite_pal "sprite_pal.png" NONE
