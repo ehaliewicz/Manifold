5156f6564295d944b0614822737bd1325bf522b3
Erik Haliewicz <ehal256@gmail.com>
2022-07-31 22:28:24 -0700

    delete some stuff

--- src/ball.h
+++
@@ -1,6 +0,0 @@
-#ifndef BALL_H
-#define BALL_H
-
-const uint8_t small_ball_sprite[17];
-
-#endif
--- src/torus_map.h
+++
@@ -1,9 +0,0 @@
-#ifndef TORUS_MAP
-#define TORUS_MAP
-
-#include "portal_map.h"
-
-
-extern const portal_map torus_map;
-
-#endif
