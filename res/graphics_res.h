eee883f9aa439e0ac58860ba0faa9306aeaf2b77
Erik Haliewicz <ehal256@gmail.com>
2022-07-31 22:29:25 -0700

    cleanup res files

--- res/graphics_res.h
+++ res/graphics_res.h
@@ -1,9 +1,7 @@
 #ifndef _RES_GRAPHICS_RES_H_
 #define _RES_GRAPHICS_RES_H_
 
-extern const Palette auto_distance_lighting_pal;
-extern const Palette no_distance_lighting_pal;
-extern const SpriteDefinition shotgun;
+extern const Palette two_light_levels_pal;
 extern const Image skybox_gradient;
 
 #endif // _RES_GRAPHICS_RES_H_
--- res/graphics_res.res
+++ res/graphics_res.res
@@ -1,5 +1,2 @@
-
-PALETTE auto_distance_lighting_pal "gritty_palette.png" NONE
-PALETTE no_distance_lighting_pal "no_dist_lighting_palette.png" NONE
+PALETTE two_light_levels_pal "two_light_levels_pal.png" NONE
 IMAGE skybox_gradient "images/SKYBOX1.png" NONE ALL
-SPRITE shotgun "images/SHOTGUN.png" 8 8 0 0 NONE TILE
