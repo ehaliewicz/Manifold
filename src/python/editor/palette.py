05827255de39c7d2773bf70fe893b2c83fd7a830
Erik Haliewicz <ehal256@gmail.com>
2022-10-09 16:03:19 -0700

    Fix palette order

--- src/python/editor/palette.py
+++ src/python/editor/palette.py
@@ -13,45 +13,35 @@ VDPPALETTE_COLORMASK = 0x0EEE
 
 col_lut = [0, 36, 72, 108, 144, 180, 216, 252]
  
+def hex_color_to_rgb(hex):
+    r = ((hex>>16)&0xFF)/256
+    g = ((hex>>8)&0xFF)/256
+    b = ((hex>>0)&0xFF)/256
+    return [r,g,b]
+
 def vdp_color_to_rgb(vdpcolor):
     r = col_lut[((vdpcolor>>1)&7)]/255
     g = col_lut[((vdpcolor>>5)&7)]/255
     b = col_lut[((vdpcolor>>9)&7)]/255
     return [r,g,b]
 
-DEFAULT_PALETTE = [vdp_color_to_rgb(rgb) for rgb in [
-    3598,
-    0,
-    0,
-    520,
-    546,
-    1090,
-    44,
-    2628,
-    98,
-    1096,
-    2690,
-    1676,
-    172,
-    2222,
-    0,
-    2278
-    #[255,  0,247],
-    #[  0,  0,  0],
-    #[0,   31, 31],
-    #[155,  0, 62],
-    #[ 62, 62, 62],
-    #[ 62, 93, 93],
-    #[217, 62,  0],
-    #[ 93, 93,186],
-    #[ 62,124,  0],
-    #[155, 93, 93],
-    #[ 62,155,186],
-    #[217,155,124],
-    #[217,186,  0],
-    #[248,186,155],
-    #[  0,  0,  0],
-    #[124,248,155],
+DEFAULT_PALETTE = [hex_color_to_rgb(rgb) for rgb in [
+    0x000000,
+    0x9b5d5d,
+    0xd99b7c,
+    0xf8ba9b,
+    0x7cf89b,
+    0x3e5d5d,
+    0x001f1f,
+    0x3e7c00,
+    0x3e3e3e,
+    0xd93e00,
+    0x1f001f,
+    0x9b003e,
+    0x3e9bba,
+    0x5d5dba,
+    0xd9ba00,
+    0x000000,
 ]]
 
 def rgb24_to_vdp_color(color):
